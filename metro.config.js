73db3f0ea810a991da20d348a26ec9391e2be292
junseopsan <junseopsan@gmail.com>
2025-08-13 20:43:12 +0900

    fix: Metro 설정 수정
    - metro.config.js: Tamagui 플러그인 제거로 Metro 버전 충돌 해결

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,5 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withTamagui } = require("@tamagui/metro-plugin");
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withTamagui(config, {
-  components: ["tamagui"],
-  config: "./tamagui.config.ts",
-  logTimings: true,
-});
+module.exports = config;
